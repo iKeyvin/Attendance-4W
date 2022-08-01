3bd0d7db1a409643820b834b711094e0a9f962de
Richard Correa <richardcorrea2306@gmail.com>
2022-08-01 23:44:00 +0200

    Added events router

--- src/server/src/entities/events/events.router.js
+++ src/server/src/entities/events/events.router.js
@@ -1,13 +1,104 @@
 import express from 'express';
-import Events  from './events.model.js';
+import Events from './events.model.js';
 
 const eventsRouter = express.Router();
 
 eventsRouter.route('/')
-.get(async (req, res, next) => {
-    const results = await Events.findAll();
+    .get(async (req, res, next) => {
+        try {
+            const filters = {
+                limit: req.query.limit || 10,
+                offset: ((req.query.page - 1) * 10),
+                subQuery: false
+            };
 
-    res.json(results);
-});
+            const results = await Events.findAll(filters);
+
+            res.status(200).json(results);
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(400);
+        }
+    })
+    .post(async (req, res, next) => {
+        try {
+            await Events.create(req.body);
+
+            res.sendStatus(201);
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(400);
+        }
+    })
+    .delete(async (req, res, next) => {
+        try {
+            const count = await Events.destroy({
+                where: {},
+                force: true
+            });
+
+            let status = count > 0 ? 204 : 404;
+            res.sendStatus(status);
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(500);
+        }
+    });
+
+eventsRouter.route('/:event_name')
+    .get(async (req, res, next) => {
+        try {
+            const filters = {
+                event_name: req.params.event_name
+            };
+
+            const result = await Events.findOne({
+                where: filters
+            });
+
+            let status = result != null ? 200 : 404;
+            res.status(status).json(result);
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(500);
+        }
+    })
+    .patch(async (req, res, next) => {
+        try {
+            const count = await Events.update(req.body, {
+                where: {
+                    event_name: req.params.event_name
+                }
+            });
+
+            let status = count > 0 ? 200 : 404;
+            res.status(status).json({ rows_affected: count[0] });
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(500);
+        }
+    })
+    .delete(async (req, res, next) => {
+        try {
+            const count = await Events.destroy({
+                where: {
+                    event_name: req.params.event_name
+                }
+            });
+
+            let status = count > 0 ? 204 : 404;
+            res.sendStatus(status);
+        }
+        catch (error) {
+            console.error(error);
+            res.sendStatus(500);
+        }
+    });
+
 
 export default eventsRouter;
--- src/server/src/entities/events/events.spec.js
+++ src/server/src/entities/events/events.spec.js
@@ -1,9 +1,97 @@
-import Event from './events.model.js';
+import request from 'supertest';
+import app from '../../app.js';
+import connection from '../../dbconnection/postgresql.js';
+
+/* #region Sample inputs */
+
+const new_event_1 = {
+    event_name:"event_test",
+    locale_id:1
+}
+
+const new_event_2 = {
+    event_name:"event_test_2",
+    locale_id:1
+}
+
+const new_event_1_updated = {
+    event_name:"event_test_updated"
+}
+/* #endregion */
+
+/* #region Before and after */
+beforeAll(async () => {
+
+});
+
+afterAll(done => {
+    connection.close();
+    done();
+});
+/* #endregion */
+
+/* #region Tests */
+describe('POST /events', () => {
+    test('should insert correctly', async () => {
+        let response = await request(app).post('/events').send(new_event_1);
+        expect(response.statusCode).toBe(201);
+    });
+
+    test('should accept new events in same locale', async () => {
+        let response = await request(app).post('/events').send(new_event_2);
+        expect(response.statusCode).toBe(201);
+    });
+
+    test('should not accept duplicates', async () => {
+        let response = await request(app).post('/events').send(new_event_2);
+        expect(response.statusCode).toBe(400);
+    });
+});
 
 describe('GET /events', () => {
-        test('should return all events', async () => {
-            let events = await Event.findAll();
-            expect(events.length > 0).toBe(true);
-        });
-    }
-);
+    test('should be OK', async () => {
+        let response = await request(app).get('/events?page=1').send();
+
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('should limit results', async () => {
+        let limit = 1;
+        let response = await request(app).get('/events?page=1&limit=' + limit).send();
+
+        expect(response.body.length).toBeLessThanOrEqual(limit);
+    });
+});
+
+describe('GET /events/:event_name', () => {
+    test('should get single user', async () => {
+        let response = await request(app).get('/events/event_test').send();
+
+        expect(response.body.event_name).toBe('event_test');
+    });
+
+    test('should fail if single user does not exist', async () => {
+        let response = await request(app).get('/events/bad_test').send();
+
+        expect(response.statusCode).toBe(404);
+    });
+});
+
+describe('PATCH /events/:event_name', () => {
+    test('should update', async () => {
+        await request(app).patch('/events/event_test').send(new_event_1_updated);
+        let response = await request(app).get('/events/event_test_updated').send();
+
+        expect(response.body.event_name).toBe('event_test_updated');
+    });
+});
+
+describe('DELETE /events/:event_name', () => {
+    test('should delete event by name', async () => {
+        await request(app).delete('/events/event_test_2').send();
+        let response = await request(app).delete('/events/event_test_updated').send();
+
+        expect(response.statusCode).toBe(204);
+    });
+});
+/* #endregion */
